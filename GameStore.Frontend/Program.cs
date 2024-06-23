4de6fc2bb369cb34d2ae5e3682597010c95fc1f7
Skeeter Man <Michael_Newman@Yamaha-Motor.com>
2024-06-23 12:56:07 -0500

    added user interactivity

--- GameStore.Frontend/Program.cs
+++ GameStore.Frontend/Program.cs
@@ -4,9 +4,16 @@ using GameStore.Frontend.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents();
-builder.Services.AddSingleton<GamesClient>();
-builder.Services.AddSingleton<GenresClient>();
+builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+var gameStoreApiUrl = "http://localhost:5141";
+
+builder.Services.AddHttpClient<GamesClient>(
+    client => client.BaseAddress = new Uri(gameStoreApiUrl));
+
+builder.Services.AddHttpClient<GenresClient>(
+    client => client.BaseAddress = new Uri(gameStoreApiUrl));
 
 var app = builder.Build();
 
@@ -23,6 +30,7 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+   .AddInteractiveServerRenderMode();
 
 app.Run();
